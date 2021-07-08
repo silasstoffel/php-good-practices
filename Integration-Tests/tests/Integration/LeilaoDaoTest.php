75923b2e207215874af31d837040052d01505b44
Silas Stoffel <silasstofel@gmail.com>
2021-07-07 21:33:09 -0300

    remove  database records after tests

--- Integration-Tests/tests/Integration/LeilaoDaoTest.php
+++ Integration-Tests/tests/Integration/LeilaoDaoTest.php
@@ -12,17 +12,23 @@ class LeilaoDaoTest extends TestCase
 {
     public function testInsertAndSearchNeedWorks()
     {
+        // arrange
+        $pdo = ConnectionCreator::getConnection();
         $description = 'Tempra Turbo 2.3';
         $entity = new Leilao($description);
-        $entityDao = new EntityDao(
-            ConnectionCreator::getConnection()
-        );
+        $entityDao = new EntityDao($pdo);
         $entityDao->salva($entity);
 
+        // act
         $items = $entityDao->recuperarNaoFinalizados();
+
+        // asserts
         self::assertCount(1, $items);
         self::assertContainsOnlyInstancesOf(Leilao::class, $items);
         self::assertSame($description, $items[0]->recuperarDescricao());
+
+        // tear down
+        $pdo->exec('DELETE FROM leiloes');
     }
 
 }
