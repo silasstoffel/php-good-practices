ea0f105001b5e47462859b4908c573253c55dad9
Silas Stoffel <silasstofel@gmail.com>
2021-07-07 21:42:59 -0300

    implements transactions on tests

--- Integration-Tests/tests/Integration/LeilaoDaoTest.php
+++ Integration-Tests/tests/Integration/LeilaoDaoTest.php
@@ -6,17 +6,33 @@ namespace Alura\Leilao\Tests\Integration\Data;
 use Alura\Leilao\Model\Leilao;
 use Alura\Leilao\Dao\Leilao as EntityDao;
 use Alura\Leilao\Infra\ConnectionCreator;
+use PDO;
 use PHPUnit\Framework\TestCase;
 
 class LeilaoDaoTest extends TestCase
 {
+
+
+    private \PDO $pdo;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->pdo = ConnectionCreator::getConnection();
+        $this->pdo->beginTransaction();
+    }
+
+    protected function tearDown(): void
+    {
+        $this->pdo->rollBack();
+    }
+
     public function testInsertAndSearchNeedWorks()
     {
         // arrange
-        $pdo = ConnectionCreator::getConnection();
         $description = 'Tempra Turbo 2.3';
         $entity = new Leilao($description);
-        $entityDao = new EntityDao($pdo);
+        $entityDao = new EntityDao($this->pdo);
         $entityDao->salva($entity);
 
         // act
@@ -26,9 +42,6 @@ class LeilaoDaoTest extends TestCase
         self::assertCount(1, $items);
         self::assertContainsOnlyInstancesOf(Leilao::class, $items);
         self::assertSame($description, $items[0]->recuperarDescricao());
-
-        // tear down
-        $pdo->exec('DELETE FROM leiloes');
     }
 
 }
