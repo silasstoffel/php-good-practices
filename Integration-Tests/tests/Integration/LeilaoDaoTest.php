45b6e54600b55fca671fc15f70b43ef96a07f7d3
Silas Stoffel <silasstofel@gmail.com>
2021-07-08 22:40:27 -0300

    Refactor(tests): LeilaoDao

--- Integration-Tests/tests/Integration/LeilaoDaoTest.php
+++ Integration-Tests/tests/Integration/LeilaoDaoTest.php
@@ -10,9 +10,7 @@ use PHPUnit\Framework\TestCase;
 
 class LeilaoDaoTest extends TestCase
 {
-
-
-    private static \PDO $pdo;
+    private static PDO $pdo;
 
     public static function setUpBeforeClass(): void
     {
@@ -33,13 +31,16 @@ class LeilaoDaoTest extends TestCase
         static::$pdo->rollBack();
     }
 
-    public function testInsertAndSearchNeedWorks()
+    /**
+     * @dataProvider auctions
+     */
+    public function testSearchUnfinishedAuctions(array $auctions)
     {
-        // arrange
-        $description = 'Tempra Turbo 2.3';
-        $entity = new Leilao($description);
         $entityDao = new EntityDao(static::$pdo);
-        $entityDao->salva($entity);
+        /**@var Leilao[] $auctions */
+        foreach ($auctions as $auction) {
+            $entityDao->salva($auction);
+        }
 
         // act
         $items = $entityDao->recuperarNaoFinalizados();
@@ -47,7 +48,46 @@ class LeilaoDaoTest extends TestCase
         // asserts
         self::assertCount(1, $items);
         self::assertContainsOnlyInstancesOf(Leilao::class, $items);
-        self::assertSame($description, $items[0]->recuperarDescricao());
+        self::assertSame(
+            $auctions[0]->recuperarDescricao(),
+            $items[0]->recuperarDescricao()
+        );
+    }
+
+    /**
+     * @dataProvider auctions
+     */
+    public function testSearchFinishedAuctions(array $auctions)
+    {
+        $entityDao = new EntityDao(static::$pdo);
+        /**@var Leilao[] $auctions */
+        foreach ($auctions as $auction) {
+            $entityDao->salva($auction);
+        }
+
+        // act
+        $items = $entityDao->recuperarFinalizados();
+
+        // asserts
+        self::assertCount(1, $items);
+        self::assertContainsOnlyInstancesOf(Leilao::class, $items);
+        self::assertSame(
+            $auctions[1]->recuperarDescricao(),
+            $items[0]->recuperarDescricao()
+        );
+    }
+
+    public function auctions(): array
+    {
+        $notFinished = new Leilao('Tempra Turbo 2.3');
+        $finished = new Leilao('Uno Turbo GT');
+        $finished->finaliza();
+
+        return [
+            [
+                [$notFinished, $finished]
+            ]
+        ];
     }
 
 }
